99b3ebfd8531182e935bca5ad5bfad678b7ba45b
MarinaNakvas <marfuny51@gmail.com>
2020-02-08 20:08:11 +0300

    last

--- ishop/ishop.js
+++ ishop/ishop.js
@@ -13,13 +13,13 @@ var Ishop = React.createClass({
             var productCode = 
             React.DOM.div({key:product.code, className: 'Product'},
                 React.DOM.span({className:'PrName'}, product.nameproduct),
-                React.DOM.span({className:'Price'}, product.price),
-                React.DOM.span({className:'Stock'}, product.stock),
-                React.DOM.div({className:'Foto'}, product.foto),
+                React.DOM.span({className:'Price'}, 'Price: ' + product.price),
+                React.DOM.span({className:'Stock'}, 'Stock: ' + product.stock),
+                React.DOM.img({className:'Foto', src: product.foto}),
             );
             productsCode.push(productCode);
         }
-        return React.Dom.div( {className:'Ishop'},
+        return React.DOM.div( {className:'Ishop'},
             React.DOM.div( {className:'NameShop'}, this.props.nameShop),
             React.DOM.div( {className:'Products'}, productsCode),
         );
