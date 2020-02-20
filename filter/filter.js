d41648429ca7d54c10f9986b2f80e767798c7824
MarinaNakvas <marfuny51@gmail.com>
2020-02-20 16:22:42 +0200

    sort3

--- filter/filter.js
+++ filter/filter.js
@@ -7,26 +7,33 @@ var Filter = React.createClass ({
         enterText: React.PropTypes.string,
     },
 
-    sortList: function(EO) {
-        this.setState( {langs:langArr.sort()} );
+    getInitialState: function () {
+        return { 
+            ourList: this.langs,
+            sortLangs: this.props.isSotr,
+            lineLangs:this.props.enterTeaxt,
+          };
     },
-
-    findInList: function() {
-
+      
+    processList: function() {
+        var lines=this.props.langs.slice();
+        if (this.state.lineLangs) 
+            lines = lines.filter(line => line.indexOf(this.state.lineLangs)!=-1);
+        if (this.state.sort)
+            lines.sortLangs();
+        this.setState({lines:lines});
     },
 
     render: function() {
-        
         var langsCode=[];
         langArr.forEach(lang => {
             var langCode = 
             React.DOM.p({key:lang.code, className:'Lang'}, lang.namelang);
             langsCode.push(langCode);
         });
-
         return React.DOM.div( {className:'Filter'},
-            React.DOM.input({type:'checkbox', onClick:this.sortList}),
-            React.DOM.input({type:'text', className: 'Text', onChange:this.findInList}),
+            React.DOM.input({type:'checkbox'}, this.sortLangs),
+            React.DOM.input({type:'text', className: 'Text'}, this.lineLangs),
             React.DOM.input({type:'button', value:'сброс'}),
             React.DOM.div({className:'List'}, langsCode),
         );
