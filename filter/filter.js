464d9faae19fb7e0798d813b94f4b58d00a72967
MarinaNakvas <marfuny51@gmail.com>
2020-02-20 19:37:13 +0300

    filter

--- filter/filter.js
+++ filter/filter.js
@@ -6,35 +6,50 @@ var Filter = React.createClass ({
         code: React.PropTypes.number,
         enterText: React.PropTypes.string,
     },
-
+    
     getInitialState: function () {
         return { 
-            ourList: this.langs,
+            ourList: this.props.langs,
             sortLangs: this.props.isSotr,
             lineLangs:this.props.enterTeaxt,
           };
     },
       
     processList: function() {
-        var lines=this.props.langs.slice();
+        var langs = this.props.langs.slice();
         if (this.state.lineLangs) 
-            lines = lines.filter(line => line.indexOf(this.state.lineLangs)!=-1);
-        if (this.state.sort)
-            lines.sortLangs();
-        this.setState({lines:lines});
+            langs = langs.filter(lang => lang.indexOf(this.state.lineLangs)!=-1);
+        if (this.state.sortLangs)
+            langs.sort();
+        this.setState({langs:langs});
+    },
+
+    cbSortLangs: function() {
+        this.setState({sortLangs: true});
+        this.processList;
+    },
+
+    cblineLangs: function(EO) {
+        this.setState({lineLangs: EO.target.value});
+        this.processList;
+    },
+
+    cbReturn: function() {
+        this.setState({sortLangs: false, lineLangs: this.props.enterTeaxt});
+        this.processList;
     },
 
     render: function() {
         var langsCode=[];
-        langArr.forEach(lang => {
+        this.props.langs.forEach(lang => {
             var langCode = 
             React.DOM.p({key:lang.code, className:'Lang'}, lang.namelang);
             langsCode.push(langCode);
         });
         return React.DOM.div( {className:'Filter'},
-            React.DOM.input({type:'checkbox'}, this.sortLangs),
-            React.DOM.input({type:'text', className: 'Text'}, this.lineLangs),
-            React.DOM.input({type:'button', value:'сброс'}),
+            React.DOM.input({type:'checkbox', onClick:this.cbSortLangs}, this.sortLangs),
+            React.DOM.input({type:'text', className: 'Text', onChange:this.cblineLangs}, this.lineLangs),
+            React.DOM.input({type:'button', value:'сброс', onClick:this.cbReturn}),
             React.DOM.div({className:'List'}, langsCode),
         );
     },
