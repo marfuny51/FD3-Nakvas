baf85410eb0a197a849ea1683177d8aa8dd37cf8
MarinaNakvas <marfuny51@gmail.com>
2020-02-22 22:17:34 +0300

    table

--- ishop2/ishop2.js
+++ ishop2/ishop2.js
@@ -14,14 +14,43 @@ var Ishop2 = React.createClass ({
     )
     },
 
-    render: function() {
+    getInitialState: function() {
+        return { 
+          selectedChooseLine: null,
+          
+        };
+      },
 
-        var productsCode = this.props.products.map( v =>
-            React.createElement(Goods, {key:v.code, nameproduct:v.nameproduct, price:v.price, url:v.url, stock:v.stock})
-        );
+    lineSelected: function(code) {
+        console.log('выбран строка с кодом '+ code);
+        this.setState( {selectedChooseLine:code} );
+      },
+
+    render: function() {
+        var productsCode=this.props.products.map( v =>
+            React.createElement(Goods, {key:v.code,
+                nameproduct:v.nameproduct, price:v.price, code:v.code,
+                url:v.url, stock:v.stock,
+                cbSelected:this.lineSelected,
+                selectedLineCode:this.state.selectedLineCode, 
+            })
+          );
+        
         return React.DOM.div({className:'Ishop2'},
         React.DOM.div( {className:'NameShop'}, this.props.nameShop),
-        React.DOM.table( {className:'Goods'}, productsCode),
+        React.DOM.div({className:'MainCont'},
+        React.DOM.table( {className:'Title'}, 
+            React.DOM.tr({className:'Title2'},
+                React.DOM.th({className:'PrName'}, 'Name'),
+                React.DOM.th({className:'Price'}, 'Price'),
+                React.DOM.th({className:'URL'}, 'URL'),
+                React.DOM.th({className:'Stock'}, 'Quantity'),
+                React.DOM.th({className:'Control'}, 'Control'),
+            ),
+        ),
+        React.DOM.table( {className:'GoodsTable'}, productsCode),
+
+        ),
         );
     },
 });
