2d29e5698f4696efa0ced6304cf1cc2e887725a7
MarinaNakvas <marfuny51@gmail.com>
2020-03-17 20:38:34 +0300

    proba4

--- RainbowFrameHOC/components/withRainbowFrame.js
+++ RainbowFrameHOC/components/withRainbowFrame.js
@@ -3,15 +3,17 @@ import React from 'react';
 
 
 function withRainbowFrame(colors) {
-  return function(Component) {
-    return props => (
-      colors.forEach(color => {
+  colors.forEach(color => {
+    return function(Component) {
+      return props => (
         <div style={{padding: '10px', textAlign: 'center', border: 'solid 5px ' + color}}>
         <Component {...props} />
         </div>
-    })
-    );
-};
+      )
+    }
+  })
+
+
 }
 
 export { withRainbowFrame };
