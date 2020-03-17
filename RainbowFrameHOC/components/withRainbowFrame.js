969b2533a96e3e91c119f8c0033952290080e302
MarinaNakvas <marfuny51@gmail.com>
2020-03-17 21:43:22 +0300

    deletecomp

--- RainbowFrameHOC/components/withRainbowFrame.js
+++ RainbowFrameHOC/components/withRainbowFrame.js
@@ -1,17 +1,16 @@
 import React from 'react';
 
-let colors=['red','orange', 'yellow','green', '#00BFFF', 'blue', 'purple'];
-function withRainbowFrame(colors) {
+function withRainbowFrame(color) {
     return function(Component) {
-      for (i=0, i<colors.length) {
+    
       return props => (
          
-        <div style={{padding: '10px', textAlign: 'center', border: 'solid 5px ' + color[i]}}>
+        <div style={{padding: '10px', textAlign: 'center', border: 'solid 5px ' + color}}>
         <Component {...props} />
         </div>
         
       )
-    }
+    
   }
 
 }
