66696d3152a6f01da3278422d7c5643c0552b676
MarinaNakvas <marfuny51@gmail.com>
2020-03-17 21:41:25 +0300

    proba5

--- RainbowFrameHOC/App.js
+++ RainbowFrameHOC/App.js
@@ -3,11 +3,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import RainbowFrameHOC  from './components/RainbowFrameHOC';
 import { withRainbowFrame } from './components/withRainbowFrame';
 
 let colors=['red','orange', 'yellow','green', '#00BFFF', 'blue', 'purple'];
-let FramedFragment=withRainbowFrame(colors)(RainbowFrameHOC);
+let FramedFragment = withRainbowFrame(colors)(React.Fragment);
 
 ReactDOM.render(
   <FramedFragment>
--- RainbowFrameHOC/components/withRainbowFrame.js
+++ RainbowFrameHOC/components/withRainbowFrame.js
@@ -1,18 +1,18 @@
 import React from 'react';
 
-
-
+let colors=['red','orange', 'yellow','green', '#00BFFF', 'blue', 'purple'];
 function withRainbowFrame(colors) {
-  colors.forEach(color => {
     return function(Component) {
+      for (i=0, i<colors.length) {
       return props => (
-        <div style={{padding: '10px', textAlign: 'center', border: 'solid 5px ' + color}}>
+         
+        <div style={{padding: '10px', textAlign: 'center', border: 'solid 5px ' + color[i]}}>
         <Component {...props} />
         </div>
+        
       )
     }
-  })
-
+  }
 
 }
 
