f8f49240fb042a9fbbd392b533d521e591155aec
MarinaNakvas <marfuny51@gmail.com>
2020-04-13 14:29:22 +0300

    interface

---
+++ Scales2/app.js
@@ -0,0 +1,82 @@
+var Scales = /** @class */ (function () {
+    function Scales() {
+        this.products = [];
+    }
+    Scales.prototype.add = function (product) {
+        this.products.push(product);
+    };
+    Scales.prototype.getNameList = function () {
+        var names = [];
+        for (var i = 0; i < this.products.length; i++) {
+            names.push(this.products[i].getName());
+        }
+        return names;
+    };
+    Scales.prototype.getSumScale = function () {
+        var scales = 0;
+        for (var i = 0; i < this.products.length; i++) {
+            scales += this.products[i].getScale();
+        }
+        return scales;
+    };
+    return Scales;
+}());
+var Product = /** @class */ (function () {
+    function Product(_name, _weight) {
+        this.name = _name;
+        this.weight = _weight;
+    }
+    Product.prototype.getName = function () {
+        return this.name;
+    };
+    Product.prototype.getScale = function () {
+        return this.weight;
+    };
+    return Product;
+}());
+var Apple = /** @class */ (function () {
+    function Apple(_name, _weight) {
+        this.name = _name;
+        this.weight = _weight;
+    }
+    Apple.prototype.getName = function () {
+        return this.name;
+    };
+    Apple.prototype.getScale = function () {
+        return this.weight;
+    };
+    return Apple;
+}());
+var Tomato = /** @class */ (function () {
+    function Tomato(_name, _weight) {
+        this.name = _name;
+        this.weight = _weight;
+    }
+    Tomato.prototype.getName = function () {
+        return this.name;
+    };
+    Tomato.prototype.getScale = function () {
+        return this.weight;
+    };
+    return Tomato;
+}());
+var firstScales = new Scales;
+var apple1 = new Apple("Antonovka", 10);
+var apple2 = new Apple("Gloster", 5);
+var apple3 = new Apple("RedPrince", 7);
+var apple4 = new Apple("Djonagored", 10);
+var tomato1 = new Tomato("Red", 2);
+var tomato2 = new Tomato("Yellow", 3);
+var tomato3 = new Tomato("Black", 8);
+var tomato4 = new Tomato("Cherry", 0.5);
+firstScales.add(apple1);
+firstScales.add(apple2);
+firstScales.add(apple3);
+firstScales.add(apple4);
+firstScales.add(tomato1);
+firstScales.add(tomato2);
+firstScales.add(tomato3);
+firstScales.add(tomato4);
+console.log('List of all products: ' + firstScales.getNameList());
+console.log('Total weight of all products: ' + firstScales.getSumScale());
+//# sourceMappingURL=app.js.map
---
+++ Scales2/app.ts
@@ -0,0 +1,112 @@
+interface IScalable {
+    
+    getName():string;
+    getScale():number;
+
+}
+
+class Scales {
+
+    products:Product[]=[];
+
+    add(product:Product):void {
+        this.products.push(product);
+    }
+
+    getNameList():string[] {
+        let names:string[]=[];
+        for(let i:number=0; i<this.products.length; i++) {
+            names.push(this.products[i].getName());
+        }
+        return names;
+    }
+
+    getSumScale():number {
+        let scales:number=0;
+        for(let i:number=0; i<this.products.length; i++) {
+            scales += this.products[i].getScale();
+        }
+        return scales;
+    }   
+}
+
+class Product implements IScalable {
+
+    name:string;
+    weight:number;
+    
+    constructor(_name:string, _weight:number) {
+        this.name=_name;
+        this.weight=_weight;
+    }
+
+    getName():string {
+        return this.name;
+    }
+
+    getScale():number {
+        return this.weight;
+    }
+}
+
+class Apple implements IScalable {
+
+    name:string;
+    weight:number;
+        
+    constructor(_name:string,_weight:number) {
+        this.name=_name;
+        this.weight=_weight;
+    }
+
+    getName():string {
+        return this.name;
+    }
+
+    getScale():number {
+        return this.weight;
+    }
+}
+
+class Tomato implements IScalable {
+
+    name:string;
+    weight:number;
+        
+    constructor(_name:string,_weight:number) {
+        this.name=_name;
+        this.weight=_weight;
+    }
+
+    getName():string {
+        return this.name;
+    }
+
+    getScale():number {
+        return this.weight;
+    }
+}
+
+let firstScales:Scales = new Scales;
+
+let apple1:Apple = new Apple ("Antonovka", 10);
+let apple2:Apple = new Apple ("Gloster", 5);
+let apple3:Apple = new Apple ("RedPrince", 7);
+let apple4:Apple = new Apple ("Djonagored", 10);
+
+let tomato1:Tomato = new Tomato ("Red", 2);
+let tomato2:Tomato = new Tomato ("Yellow", 3);
+let tomato3:Tomato = new Tomato ("Black", 8);
+let tomato4:Tomato = new Tomato ("Cherry", 0.5);
+
+firstScales.add(apple1);
+firstScales.add(apple2);
+firstScales.add(apple3);
+firstScales.add(apple4);
+firstScales.add(tomato1);
+firstScales.add(tomato2);
+firstScales.add(tomato3);
+firstScales.add(tomato4);
+
+console.log('List of all products: ' + firstScales.getNameList());
+console.log('Total weight of all products: ' + firstScales.getSumScale());
