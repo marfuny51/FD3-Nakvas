79e286f1d6df592b6aab80892ab5a9bf0cabdab5
MarinaNakvas <marfuny51@gmail.com>
2020-05-12 13:00:59 +0200

    packets

---
+++ Cinema/src/app/app.module.ts
@@ -0,0 +1,15 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  imports: [ BrowserModule, FormsModule ],
+  declarations: [
+    
+  ],
+  providers: [
+    
+  ],
+  bootstrap: []
+})
+export class AppModule { }
