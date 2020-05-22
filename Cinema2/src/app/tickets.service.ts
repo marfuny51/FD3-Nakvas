f3e498405d30f727566df491232e9863840083cd
MarinaNakvas <marfuny51@gmail.com>
2020-05-22 13:38:10 +0200

    observable

--- Cinema2/src/app/tickets.service.ts
+++ Cinema2/src/app/tickets.service.ts
@@ -4,49 +4,27 @@ import {Observable} from 'rxjs';
 @Injectable()
 export class Tickets {
 
-  private places:Array<{number:number, free:boolean}>=[
-    { number:1, free: true },
-    { number:2, free: true },
-    { number:3, free: true },
-    { number:4, free: true },
-    { number:5, free: true },
-    { number:6, free: true },
-    { number:7, free: true },
-    { number:8, free: true },
-    { number:9, free: true },
-    { number:10, free: true },
-    { number:11, free: true },
-    { number:12, free: true },
-    { number:13, free: true },
-    { number:14, free: true },
-    { number:15, free: true },
-    { number:16, free: true },
-    { number:17, free: true }
-  ];
-
-  
-  private places2:Array<boolean> = [true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true];
-  private places2$:Observable<Array<boolean>>;
+  private places:Array<boolean> = [true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true];
+  private places$:Observable<Array<boolean>>;
 
   constructor() {
-    this.places2$ = new Observable<Array<boolean>>(event=> event.next(this.places2))
+    this.places$ = new Observable<Array<boolean>>(event=> event.next(this.places))
   }
 
   getPlacesObservable():Observable<Array<boolean>> {
-    return this.places2$;
+    return this.places$;
   }
 
   getNumber(count:number):any {
-    let places = [...this.places];
     let arrTickets:Array<number> = [];
     let num:number = 0;
     if (count <= this.getFree()) {
-      for (let i:number=0; i < places.length; i++) {
+      for (let i:number=0; i < this.places.length; i++) {
         if (num < count) {
-          if (places[i].free === true) {
-            places[i].free = false;
+          if (this.places[i] === true) {
+            this.places[i] = false;
             num += 1;
-            arrTickets.push(places[i].number)
+            arrTickets.push(i+1)
           }
         }
       }
@@ -62,9 +40,14 @@ export class Tickets {
   };
 
   getFree():number {
+    let freePlace:Array<boolean> = [];
     let places = [...this.places];
-    let freePlaces = places.filter(place => place.free===true).length;
-    return freePlaces;
+    places.forEach((place) => {
+      if(place === true) {
+        freePlace.push(place)
+      }
+    });
+    return freePlace.length;
   };
 
 }
